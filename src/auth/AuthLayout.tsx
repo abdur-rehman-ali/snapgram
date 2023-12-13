de6b9bba645fe4fbdb89dcfa8b52e8763a655d64
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2023-12-13 14:29:01 -0800

    Add assets and auth layout rendering (#2)

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -1,8 +1,26 @@
+import { Outlet, Navigate } from 'react-router-dom'
+
 const AuthLayout = () => {
+  const isAuthenticated = false
   return (
-    <div>
-      <h1>This is auth layout</h1>
-    </div>
+    <>
+      {
+        isAuthenticated ?
+          (<Navigate to="/home" />) :
+          (
+            <section className='flex justify-between'>
+              <section className='border-solid border-2 w-1/2 flex flex-col justify-center items-center py-10'>
+                <Outlet />
+              </section>
+              <img
+                src='/assets/images/side-img.svg'
+                alt='Side bar image here'
+                className='hidden lg:block w-1/2 h-screen object-cover'
+              />
+            </section>
+          )
+      }
+    </>
   )
 }
 
