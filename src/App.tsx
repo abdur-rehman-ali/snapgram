995856b882ef3c28b25cfa4e4bdb6ccedbe8caee
abdur-rehman-ali <alijoyia911@gmail.com>
2023-12-12 16:11:28 -0800

    Setup basic directory structure for snapgram

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Register from './auth/forms/Register';
+import Login from './auth/forms/Login';
+import AuthLayout from './auth/AuthLayout';
+import RootLayout from './root/RootLayout';
+import { Home } from './root';
+
 const App = () => {
   return (
-    <div className="text-center">
-      <h1>Welcome to snapgram</h1>
-    </div>
+    <main>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={ <Home/>} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
