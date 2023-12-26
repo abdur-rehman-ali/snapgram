d8969aed3206cc4786dc9e0e872cfcc72fe79b33
abdur-rehman-ali <alijoyia911@gmail.com>
2023-12-26 11:51:13 -0800

    Setup shadcn

--- src/auth/forms/Login.tsx
+++ src/auth/forms/Login.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const Login = () => {
   return (
     <div>
       <h1>This is login Form</h1>
+      <Button>Click Me</Button>
     </div>
   )
 }
