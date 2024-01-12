b406ad4f24a3db22aacb7a903183fe4bc111bde7
abdur-rehman-ali <alijoyia911@gmail.com>
2024-01-12 15:07:05 -0800

    Add Create post header

--- src/root/pages/Posts/CreatePost.tsx
+++ src/root/pages/Posts/CreatePost.tsx
@@ -1,7 +1,9 @@
+import CreatePostHeader from "./CreatePostHeader"
+
 const CreatePost = () => {
   return (
     <div>
-      <h1>This is post create page</h1>
+      <CreatePostHeader/>
     </div>
   )
 }
