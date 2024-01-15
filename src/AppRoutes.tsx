853310837425a13715515750f2c42d27d8838a10
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2024-01-15 15:14:58 -0800

    Refactor routes in seprate files (#29)

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,42 @@
+import { Routes, Route } from 'react-router-dom';
+import Register from './auth/forms/Register';
+import Login from './auth/forms/Login';
+import AuthLayout from './auth/AuthLayout';
+import RootLayout from './root/RootLayout';
+import { Explore, Home, Users, Saved, CreatePost } from './root';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/posts/new' element={<CreatePost />} />
+        </Route>
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+  )
+}
+
+export default AppRoutes
