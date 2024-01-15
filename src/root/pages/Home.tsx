e115545a8a473581a421cc1450cc278680a943cb
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2024-01-15 09:00:52 -0800

    Create UI of post card (#23)

--- src/root/pages/Home.tsx
+++ src/root/pages/Home.tsx
@@ -1,7 +1,14 @@
+import PostCard from "@/components/shared/Cards/PostCard/PostCard"
+
 const Home = () => {
   return (
-    <div>
-      <h1>This is home page</h1>
+    <div className="home-posts">
+      <h1 className="h3-bold md:h2-bold text-left w-full">Home Feed</h1>
+      <ul className="flex flex-col flex-1 gap-9 w-full">
+        <li className="flex justify-center w-full">
+          <PostCard />
+        </li>
+      </ul>
     </div>
   )
 }
