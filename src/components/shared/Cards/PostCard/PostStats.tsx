eafd38b671fef58d3e8d2031986fa7b1abe08e3e
abdur-rehman-ali <alijoyia911@gmail.com>
2024-01-26 06:56:22 -0800

    style: add save post icon on post card

--- src/components/shared/Cards/PostCard/PostStats.tsx
+++ src/components/shared/Cards/PostCard/PostStats.tsx
@@ -7,7 +7,7 @@ const PostStats = ({ post }: { post: Models.Document }) => {
   const { user } = useUserContext()
   const { mutate: likePost } = useUpdateLikeOnPost()
   return (
-    <div>
+    <div className='flex justify-between'>
       <img
         src={`${isPostLiked(post, user.id) ?
           '/assets/icons/liked.svg' :
@@ -18,6 +18,13 @@ const PostStats = ({ post }: { post: Models.Document }) => {
         width={20}
         height={20}
         onClick={() => likePost({ postID: post.$id, userID: user.id })}
+      />
+      <img
+        src={'/assets/icons/save.svg'}
+        alt="save-image"
+        className='cursor-pointer'
+        width={20}
+        height={20}
       />
     </div>
   )
