6e58ae58451d700da9e2642ef25ec2f311b6acd3
abdur-rehman-ali <alijoyia911@gmail.com>
2024-02-01 15:38:13 -0800

    feat: display users on users page

--- src/root/pages/Users.tsx
+++ src/root/pages/Users.tsx
@@ -1,7 +1,25 @@
+import UserCard from "@/components/shared/Cards/users/UserCard"
+import PageHeader from "@/components/shared/Headers/PageHeader"
+import { useGetAllUsers } from "@/lib/react-query/queries"
+import { Loader } from "lucide-react"
+
 const Users = () => {
+  const { data: allUsers, isPending } = useGetAllUsers()
+
+  if (isPending) {
+    return <Loader />
+  }
+
   return (
     <div>
-      <h1>This is users page</h1>
+      <PageHeader title="All Users" headerIconUrl="/assets/icons/people.svg" />
+      <div className="pt-10 flex flex-wrap gap-4">
+        {
+          allUsers && allUsers.documents.map((user) => (
+            <UserCard user={ user} />
+          ))
+        }
+      </div>
     </div>
   )
 }
